8f0f7b83b6a4ff39a9b807e729b865065c1eebe8
UndefinedOffset <edchipman@gmail.com>
2013-07-25 16:14:14 -0300

    Field now updates on change

--- javascript/colorfield.js
+++ javascript/colorfield.js
@@ -12,6 +12,11 @@
                     },
                     onBeforeShow: function() {
                         $(this).ColorPickerSetColor(this.value);
+                    },
+                    onChange: function (hsb, hex, rgb) {
+                        var mid=(rgb.r+rgb.g+rgb.b)/3;
+                        var col=(mid>127 ? '#000000':'#ffffff');
+                        self.val(hex).css({color:col, backgroundColor:'#'+hex});
                     }
                 });
             },
